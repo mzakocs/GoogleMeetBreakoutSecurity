4a5442e0d99fb189176bd77f659d7a84294affe0
mzakocs <mzakocs@gmail.com>
2021-01-24 17:20:09 -0700

    Added basic devtool panel creation code

--- devtools.js
+++ devtools.js
@@ -1,16 +1,9 @@
 // Creates a new panel in the DevTools Page
-chrome.devtools.panels.create("My Panel",
-    "images/icon.png",
+chrome.devtools.panels.create("Google Meet Breakouts",
+    "images/icon16.png",
     "devtoolsPanel.html",
     (panel) => {
       /* Code Runs on Panel Creation */
-      // Grabs the findBreakoutRoomsButton in the DOM
-      let findBreakoutRoomsButton = document.getElementById("findBreakoutRooms");
-      // Add an event listener for when the button is clicked
-      findBreakoutRoomsButton.addEventListener("click", () => {
-        chrome.devtools.network.getHAR((harLog) => {
-            console.log(harLog)
-        })
-      });
+      
     }
 );
