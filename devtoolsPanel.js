c55c9f246780b9bec72873202c92b25b584e56cf
mzakocs <mzakocs@gmail.com>
2021-01-25 09:06:59 -0700

    v1.0 Fully Functional

--- devtoolsPanel.js
+++ devtoolsPanel.js
@@ -1,3 +1,43 @@
+/* FUNCTION DECLARATIONS */
+// Sends a command to background.js that changes the current google meet code
+const sendCodeCommand = function () {
+    let code = this;
+    // Opens a connection with background.js
+    let port = chrome.extension.connect({
+        name: "background_connection",
+    });
+    // Creates a text JSON for transmission to the background
+    let jsonText = JSON.stringify({
+        type: "changeMeetingCode",
+        code: code
+    });
+    // Sends the JSON command through the port
+    port.postMessage(jsonText);
+};
+
+// Sends a command to background.js that removes modal popup
+const sendModalCommand = function () {
+    // Opens a connection with background.js
+    let port = chrome.extension.connect({
+        name: "background_connection"
+    });
+    // Creates a text JSON for transmission to the background
+    let jsonText = JSON.stringify({
+        type: "modalRemoval"
+    });
+    // Sends the JSON command through the port
+    port.postMessage(jsonText);
+};
+
+// Adds a button to the roomDiv 
+const addRoomButton = function (title, code, div) {
+    let codeButton = document.createElement("button")
+    codeButton.innerText = title;
+    codeButton.addEventListener("click", sendCodeCommand.bind(code));
+    div.appendChild(codeButton)
+}
+
+/* RUNNING CODE */
 // Grabs the findBreakoutRoomsButton in the DOM
 let refreshButton = document.getElementById("refreshBreakoutRooms");
 // Add an event listener for when the button is clicked
@@ -18,24 +58,22 @@ refreshButton.addEventListener("click", () => {
                     // Uses an extremely rudimentary string-length filter; it would be a lot better if I
                     // knew Googles base64 encoding method, but this is the best method I came up with
                     let splitCodes = decodedContent.split("\n").filter((entry) => entry.length == 14);
-                    splitCodes.forEach(function(code, i) {
+                    splitCodes.forEach(function (code, i) {
                         splitCodes[i] = code.substr(1, code.length - 2);
                     });
                     console.log(splitCodes)
                     // Clears the room storage HTML div
                     let roomDiv = document.getElementById("roomDiv");
                     roomDiv.innerHTML = "";
-                    // // Adds a button for each meeting code
-                    // splitCodes.forEach((code, i) => {
-                    //     let codeButton = document.createElement("b")
-                    //     codeButton.innerText = "Breakout Room " + i.toString();
-                    //     codeButton.addEventListener("click", () => {
-                    //         window.open()
-                    //     });
-                    //     roomDiv.appendChild(codeButton)
-                    // });
+                    // Adds a button for each meeting code
+                    splitCodes.forEach((code, i) => {
+                        addRoomButton(("Breakout Room " + (i + 1).toString()), code, roomDiv);
+                    });
                 }
             });
         });
     })
 });
+// Add an event listener to the modal removal button
+let modalRemovalButton = document.getElementById("modalRemoval");
+modalRemovalButton.addEventListener("click", sendModalCommand);
